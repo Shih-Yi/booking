e71a466451309af5af045fd5dd66bb0d3310426e
shihyi <brvast@gmail.com>
2021-05-15 15:23:41 +1200

    add reservation before_validation :create_if_not_exist

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,18 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   accepts_nested_attributes_for :user
+  before_validation :create_if_not_exist
+
+  def create_if_not_exist
+    # Find or create the user by name
+    if new_user = User.find_by_mobile(user.mobile)
+      self.user = new_user
+    elsif user && user.persisted? && user.changed?
+      # New condition: if user is already allocated to post, but is changed, create a new user.
+      self.user = User.new(name: user.name, mobile: user.mobile)
+    else
+      # else create a new user
+      self.user.save!
+    end
+  end
 end
