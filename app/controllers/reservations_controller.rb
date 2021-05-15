b401b3346df43d297ed8b3c3e440063b7107c5eb
shihyi <brvast@gmail.com>
2021-05-15 19:44:34 +1200

    add Edit & Update & Destroy

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,6 @@
 class ReservationsController < ApplicationController
+  before_action :set_reservation, only: [:edit, :update, :destroy]
+
   def index
     @reservations = Reservation.all
     respond_to do |format|
@@ -6,6 +8,7 @@ class ReservationsController < ApplicationController
       format.json { render json: @reservations.includes(:user).to_json({include: { user: { only: [:name, :mobile]}}}) }
     end
   end
+
   def new
     @reservation = Reservation.new
     @user = User.new
@@ -18,10 +21,23 @@ class ReservationsController < ApplicationController
   end
 
   def edit
-    @reservation = Reservation.first
+  end
+
+  def update
+    @reservation.update(reservation_params)
+    redirect_to reservations_path
+  end
+
+  def destroy
+    @reservation.destroy
+    render status: 200, json: { result: "deleted successfully" }
   end
 
   private
+
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
 
   def reservation_params
     params.require(:reservation).permit(:number_of_customer, :table_number, :remark, :booking_at, user_attributes: [:name, :email, :mobile])
